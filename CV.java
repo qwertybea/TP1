c9e6ed046b3f62534c7a306b594de33d31618901
Justin Fofana <indigolasagna@gmail.com>
2018-01-29 14:57:50 -0500

    remplissage du main

--- CV.java
+++ CV.java
@@ -3,14 +3,21 @@ public class CV{
   private String nom;
   private String prenom;
   private String formation;
-  private float experiance;
+  private float experience;
   private String[] competences;
   private String attentes;
 
   public CV(){
-    
-    
-    
+    this("nom", "prenom", "formation", 0, new String[]{"competences"}, "attentes");
+  }
+  
+  public CV(String nom, String prenom, String formation, float experience, String[] competences, String attentes){
+    this.nom = nom;
+    this.prenom = prenom;
+    this.formation = formation;
+	this.experience = experience;
+	this.competences = competences;
+	this.attentes = attentes;
   }
 
   public void affiche(){
@@ -36,5 +43,19 @@ public class CV{
 }
 
 public static void main(String args[]){
+  System.out.println("Bienvenue chez Barrette!");
+  
+  CV cvJustin = new CV("Fofana", "Justin", "420.AA Informatique de gestion", 2, new String[]{"pro du vélo", "dance comme un arbre"}, "Que tous le monde s'amuse bien :)");
+  
+  CV cvYoucef = new CV("Bokari", "Oussama Youcef", "420.AA Informatique de gestion", 2, new String[]{"pro du vélo", "dance comme un crabe"}, "Que tous le monde s'amuse bien :)");
+  
+  ArrayList<CV> equipe = new ArrayList<>();
+  
+  equipe.add(cvJustin);
+  equipe.add(cvYoucef);
+  
+  for(CV cv : equipe){
+	  cv.affiche();
+  }
   
 }
